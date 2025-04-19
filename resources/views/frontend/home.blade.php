cc524c7ec983c2d32c822b7506dbcd0945a95127
Genosbanyar <bnyar012@gmail.com>
2025-04-19 21:10:03 +0630

    auth with laravel ui

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <x-slot name="title">
+        <title>Wallet</title>
+    </x-slot>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <h1>Home page</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\frontend\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
+
+Auth::routes();
+
