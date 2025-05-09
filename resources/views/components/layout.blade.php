2f578a29c836b0080c6d1775a4a34bfb492d438c
Genosbanyar <bnyar012@gmail.com>
2025-05-09 23:04:10 +0630

    scan & pay

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,14 +21,22 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
         rel="stylesheet" />
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.12.1/font/bootstrap-icons.min.css">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <script src="https://kit.fontawesome.com/225a355f8f.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+
 </head>
 
 <body>
@@ -41,6 +49,7 @@
 
     </div>
     <x-front-footer />
+
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
--- resources/views/frontend/scan_pay.blade.php
+++ resources/views/frontend/scan_pay.blade.php
@@ -4,6 +4,59 @@
     </x-slot>
     @section('header', 'Scan & Pay')
     <x-card-wrapper>
+        <div class="text-center scanPhoto">
+            <img src="{{ asset('images/qrscan.png') }}">
+        </div>
+        <p class="text-center click_scan mb-4">Click "Scan" button, put QR code in the frame and pay</p>
+        <div class="text-center">
+            <button type="button" class="btn scan-btn btn-primary" data-toggle="modal" data-target="#myModal">
+                <i class="bi bi-upc-scan"></i> Scan</button>
 
+            <!-- The Modal -->
+            <div class="modal" id="myModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                            <h4 class="modal-title">Scan & Pay</h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                            <video id="scanner"></video>
+                        </div>
+
+                        <!-- Modal footer -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </x-card-wrapper>
+    @section('script')
+        <script src="{{ asset('js/qr-scanner.umd.min.js') }}"></script>
+        <script>
+            $(document).ready(function() {
+                let videoElem = document.getElementById('scanner');
+
+                const qrScanner = new QrScanner(
+                    videoElem,
+                    function(result) {
+                        console.log(result);
+                    }, {
+                        returnDetailedScanResult: true
+                    });
+
+                $(".modal").on('shown.bs.modal', function(event) {
+                    qrScanner.start();
+                });
+
+            })
+        </script>
+    @endsection
 </x-layout>
